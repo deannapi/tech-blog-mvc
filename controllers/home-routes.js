a8efbd849abc981610f8fe55caa7aa39144ce4f5
deannapi <deannapi.mart@gmail.com>
2020-06-18 12:51:27 -0500

    added home-route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,63 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
-const sequelize = require('../config/connection');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const sequelize = require("../config/connection");
 
-// Get all 
+// Get all Posts
+router.get("/", (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "title", "post_url", "created_at"],
+    include: [{ Comment, User }],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [{ Comment, User }],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
