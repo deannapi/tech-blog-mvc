f813a09b6be9ead2ba38e030879c21563407cb75
deannapi <deannapi.mart@gmail.com>
2020-06-24 13:36:07 -0500

    changed id error

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,7 +18,7 @@ async function signupFormHandler(event) {
       
       // check the response status
       if (response.ok) {
-        console.log('success');
+        console.log('Account created successfully.');
       } else {
         alert(response.statusText);
       }
