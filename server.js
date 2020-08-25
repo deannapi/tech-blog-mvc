9143be626993afa736641238afa4bef4c0ac2d21
deannapi <deannapi.mart@gmail.com>
2020-08-25 10:47:37 -0500

    server

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const path = require('path');
-const helpers = require('./utils/helper');
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
-const session = require('express-session');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Assign Handlebars as the tempate engine
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
-const sess = {
-  secret: 'alphaDog',
-  cookie: {expires: 15 * 60 * 1000},
-  resave: true,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Add HTML cookies library
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
