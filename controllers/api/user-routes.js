d95f1f1f2cf42de72ab99d709b261da686d3f3f4
deannapi <deannapi.mart@gmail.com>
2020-08-27 20:53:03 -0500

    change

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -91,7 +91,10 @@ router.post("/login", (req, res) => {
     }
 
     req.session.save(() => {
+<<<<<<< HEAD
       // declare session variables
+=======
+>>>>>>> parent of f813a09... changed id error
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.twitter = dbUserData.twitter;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -18,6 +19,32 @@ async function loginFormHandler(event) {
       document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
+=======
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      
+      // check the response status
+      if (response.ok) {
+        console.log('success');
+      } else {
+        alert(response.statusText);
+      }
+>>>>>>> parent of f813a09... changed id error
     }
   }
 }
